61e63c6523fe1e9ad32f3a9db872e32a7f330bbc
Henrique Flores Hiemer <h.hiemer@ufn.edu.br>
2026-09-08 18:37:26 -0300

    Refactor Pessoa class with improved documentation
    Updated class documentation and added comments for clarity.

--- Aula6/src/javaapplication2/Pessoa.java
+++ Aula6/src/javaapplication2/Pessoa.java
@@ -5,29 +5,36 @@
 package javaapplication2;
 
 /**
+ * Classe simples (modelo/entidade) que representa uma pessoa cadastrada,
+ * usada tanto pela tela RadioButton quanto pela classe Arquivo.
  *
  * @author laboratorio
  */
 public class Pessoa {
     public String nome;
-    public char sexo;
+    public char sexo;    // 'M' ou 'F'
     public String idioma;
 
+    // Construtor usado tanto para criar uma pessoa nova quanto para
+    // reconstruir uma pessoa a partir dos dados lidos do arquivo
     public Pessoa(String nome, char sexo, String idioma) {
         this.nome = nome;
         this.sexo = sexo;
         this.idioma = idioma;
     }
 
+    // Usado no System.out.println(pessoa) do RadioButton, para debug no console
     @Override
     public String toString() {
         return "Pessoa{" + "nome=" + nome + ", sexo=" + sexo + ", idioma=" + idioma + '}';
     }
-    
+
+    // Converte os atributos em um array de Object, no formato que a
+    // DefaultTableModel espera para preencher uma linha da JTable
     public Object[] obterDados(){
         return new Object[] {nome, sexo, idioma};
     }
-    
-    
-    
+
+
+
 }
